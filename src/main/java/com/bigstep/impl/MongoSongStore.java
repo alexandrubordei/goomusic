6a9b12a076a9144ada323cc210d9a2986d69d94b
Alexandru Bordei <alexandru.bordei@hostway.ro>
2016-03-03 16:31:27 +0200

    Updated mongo to index based search.

--- src/main/java/com/bigstep/impl/MongoSongStore.java
+++ src/main/java/com/bigstep/impl/MongoSongStore.java
@@ -59,7 +59,7 @@ public class MongoSongStore implements SongStore {
     @Override
     public Observable<Song> getSongByArtistAsync(String query) {
         return mongoCollection
-                .find(and(lte("artist_lc", query),gte("artist_lc",query+"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")))
+                .find(and(gte("artist_lc", query),lte("artist_lc",query+"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")))
                 .toObservable()
                 .map(d -> Song.createFromJson(d.toJson()));
     }
