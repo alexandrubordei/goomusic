6e60d4f8a521a1eec617815429269ab05804076f
root <root@instance-11039.bigstep.io>
2016-03-03 14:48:28 -0500

    Updated index.html

--- run_goomusic_couchbase.sh
+++ run_goomusic_couchbase.sh
@@ -4,7 +4,7 @@ GOOMUSIC_HOME=/opt/goomusic
 
 OPTS="$OPTS -Dcom.bigstep.GoomusicSongMain.songStoreImpl=com.bigstep.impl.CouchbaseSongStore"
 OPTS="$OPTS -Dcom.bigstep.impl.CouchbaseSongStore.bucketName=lastfm"
-OPTS="$OPTS -Dcom.bigstep.impl.CouchbaseSongStore.password=lastfm"
+OPTS="$OPTS -Dcom.bigstep.impl.CouchbaseSongStore.password="
 OPTS="$OPTS -Dcom.bigstep.impl.CouchbaseSongStore.cbServers=localhost"
 echo $OPTS
 
